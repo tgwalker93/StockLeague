9ba1f83b850c49b012262d21e8f614ced5e5a2cf
tammy le <tammyle@dhcp-v122-011.mobile.uci.edu>
2017-10-11 21:02:30 -0700

    after presentation push

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,33 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// first we need to require the sequelize package
+var Sequelize = require('sequelize');
 
-var connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    connection = mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'password',
-        database:'stocks_db'
-    })
+// then set the env variable which will be either
+// production or development based on the existence
+// of the JawsDB environment variable
+var env;
+if (process.env.JAWSDB_URL)
+    env = "production";
+else {
+    env = 'development';
 }
 
+// we're going to define our config for the sql server based
+// on feeding the env variable to the config file
+var config = require('./config')[env];
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// now we will set up our connection variable (sequelize) based
+// on whether use_env_variable exists in our config
+var sequelize;
+if (config.use_env_variable) {
+	// jawsDB production option
+	sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+	// local mysqlDB development option
+	sequelize = new Sequelize(config.database, config.username, config.password, {
+		host: config.host,
+		dialect: config.dialect
+	});
+}
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// finally export the connection to the model
+module.exports = sequelize;
